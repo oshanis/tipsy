cf3eb25ac77e61fb766c6e327a091ba44090dfd3
oshanis <oshanis@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-01-20 00:32:59 +0000

    added the new firefox directory structure

---
+++ trunk/firefox/defaults/preferences/tipsy.js
@@ -0,0 +1,5 @@
+pref("extensions.tipsy.boolpref", false);
+pref("extensions.tipsy.intpref", 0);
+pref("extensions.tipsy.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.tipsy@csail.mit.edu.description", "chrome://tipsy/locale/tipsy.properties");
