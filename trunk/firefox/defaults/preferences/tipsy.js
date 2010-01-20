52e0307aeacec9c1462aa76cec932c1d5eda43dd
oshanis <oshanis@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-01-20 21:18:49 +0000

    deleted the old extension, starting from scratch

--- trunk/firefox/defaults/preferences/tipsy.js
+++
@@ -1,5 +0,0 @@
-pref("extensions.tipsy.boolpref", false);
-pref("extensions.tipsy.intpref", 0);
-pref("extensions.tipsy.stringpref", "A string");
-// See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.tipsy@csail.mit.edu.description", "chrome://tipsy/locale/tipsy.properties");
