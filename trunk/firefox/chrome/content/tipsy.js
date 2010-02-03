c0413f819d22cd05e84cbf6a8f7cfe2254f179aa
oshanis <oshanis@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-02-03 04:40:49 +0000

    added the UI skeleton, some functions, and updated the manifest so it works with FF 3.6

---
+++ trunk/firefox/chrome/content/tipsy.js
@@ -0,0 +1,44 @@
+/**
+* All the UI Interaction code goes here.
+*/
+
+/** Clears the tipsy history containing the sites visited by the user, and sites liked by the user**/
+function clearHistory(){
+    alert("clearHistory: Not Implemented!");
+}
+
+/** This function should validate the number of sites the user wishes to select through the lottery process
+Validate for high numbers like 99 and for non numbers**/
+function validateNumberOfSitesToPay(){
+    alert("validateNumberOfSitesToPay: Not Implemented!");
+}
+
+
+/**Once the user clicks on the disabled paypal account text box, this shoule be called, and it should verify with the user whether she really wants to change the PayPal account or not, and handle that event.**/
+function changePayPalAccount(){
+    alert("changePayPalAccount: Not Implemented");
+}
+
+/**Run some randomized algo, and pick the winners**/
+function runElection(){
+    alert("runElection: Not Implemented");
+}
+
+/**Validate for negative amounts. Also, may be currency convertions?*/
+function validateWebWorth(){
+    alert("validateWebWorth: Not Implemented");
+}
+
+/**
+* Validate the web worth field, the frequency of running the election (may be not required), and the PayPal account. Upon success call the relevant database update methods, otherwise notify the problem(s) to the user
+**/
+function updateInfo (){
+    alert("updateInfo: Not Implemented");
+}
+
+/**Validate the frequency of which the election is being run
+@@ We might not really need this method!
+**/
+function validateFrequency(){
+    alert("validateFrequency: Not Implemented");
+}
