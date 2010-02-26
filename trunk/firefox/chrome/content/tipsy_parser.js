427ed0793dec2d98e3a40591064af36692085ff5
marcua <marcua@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-02-26 22:29:56 +0000

    broken tipsy---doesn't get saved in window

--- trunk/firefox/chrome/content/tipsy_parser.js
+++ trunk/firefox/chrome/content/tipsy_parser.js
@@ -1,6 +1,27 @@
-
-//parse the RDFa and create a tipsy data object
-parseRDFa = function(){
+//function() {
+    var tipsy = {
+        test : function() {
+            alert('yay');
+        },
+        parseDomElement : function(selectorId){
+        },
+    };
     
-
-}
+    var tipsy_init = {
+        init: function() {
+            var appcontent = document.getElementById("appcontent");
+            if(appcontent) {
+                appcontent.addEventListener("DOMContentLoaded", tipsy_init.onPageLoad, true);
+            }
+        },
+        onPageLoad: function(aEvent) {
+            var doc = aEvent.originalTarget;
+            alert(doc.location);
+            doc.test = 'yoooo';
+            doc.tipsy = tipsy;
+            doc.defaultView.test = 'yooo';
+            alert(doc.test);
+        },
+    };
+    window.addEventListener("load", tipsy_init.init , false);
+//}();
