366ab242e2fd994c3149d1531066ce99df337b6c
oshanis <oshanis@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-05-09 17:26:17 +0000

    fixed the log function

--- firefox/chrome/content/log.js
+++ firefox/chrome/content/log.js
@@ -1,9 +1,16 @@
 // Fail gracefully if Firebug's not installed
-try {
+/*try {
     console.log('Firebug console found.');
 } catch(e) { 
     console = { 
         log: function() {} 
     };
+}
+*/
+
+//This is the correct way to log when debugging a Firefox extension -- oshani
+function log(message) {
+  var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+  consoleService.logStringMessage("Tipsy: " + message);
 }
 
