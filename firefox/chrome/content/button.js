3d4f557057cc6dea36b9d7851e8c8adde9f8ca9e
ahmad.bakhiet <ahmad.bakhiet@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-07-12 01:22:18 +0000

    Committing new files which render isolated buttons in the toolbar pallet.
    One button entitled "Me" is for configuring customer side actions and the other button entitled "Site" is for configuring vendor side actions.
    
    IMPORTANT:
    The first time the user installs they must:
    
    1) Right-click on the menu toolbar (anywhere near File/Edit/View etc)
    2) Click "Customize..."
    3) Drag each of the r-buttons to the desired location
    
    The r-buttons will now always remain at the set position unless the user chooses to "Customize..." them again.
    
    Each r-button does not process any action as of yet as I still have to build the javascript file for this.

---
+++ firefox/chrome/content/button.js
@@ -0,0 +1,15 @@
+customerbuttonjs = {
+
+1: function () {
+  alert("No JavaScript functionaility has been implemented yet - Ahmad")	
+  },
+
+}
+
+vendorbuttonjs = {
+
+1: function () {
+  alert("No JavaScript functionaility has been implemented yet - Ahmad")	
+  },
+
+}
