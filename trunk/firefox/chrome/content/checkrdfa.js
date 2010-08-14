c0ca09a6861ae1bf75d7ce847db0c917ba2b619c
ahmad.bakhiet <ahmad.bakhiet@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-08-14 18:42:15 +0000

    Created code in the new checkrdfa.js file to turn the left r-button grey/red after each event (move tab/load url). Now a condition needs to be created so that this code is only executed if RDFa exists on the page, then the r-button will only turn red in the correct scenario.

--- trunk/firefox/chrome/content/checkrdfa.js
+++ trunk/firefox/chrome/content/checkrdfa.js
@@ -1,37 +1,92 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
-<HTML>
-<head>
-<title>Order query : Firefox 1.5 Example</title>
-<style type="text/css">
-body, p {
-	font-family: Verdana, sans-serif;
-	font-size: 12px;
-   	}
-</style>
-<script type="text/javascript">
-function onLoad() {
-	loadOnlyFirst();
-}
+/*
+//Have to set the correct context for Jquery to pick things up
+jQuery.noConflict();
+$ = function(selector,context){ return new jQuery.fn.init(selector,context||window.content.document); };
+$.fn = $.prototype = jQuery.fn;
 
-function loadOnlyFirst() {
-	alert("I am now testing RDFa");
-}
-</script>
-</head>
-<body onload="onLoad();" onpageshow="if (event.persisted) onPageShow();"> 
-<h2>Order query</h2>
+var tipsy = {
 
-<form name="zipForm" action="http://www.example.com/formresult.html" method="get">
-<label for="timefield">Date and time:</label>
-<input type="text" id="timefield"><br>
-<label for="name">Name:</label>
-<input type="text" id="name"><br>
-<label for="address">Email address:</label>
-<input type="text" id="address"><br>
-<label for="order">Order number:</label>
-<input type="text" id="order"><br>
-<input type="submit" name="submit" value="Submit Query"> 
-</form>
-</body>
-</html>
+    db: null,
+
+    init: function() {
+        var appcontent = top.document.getElementById("appcontent");
+        if(appcontent) {
+            appcontent.addEventListener("DOMContentLoaded", tipsy.onPageLoad, true);
+        }
+    },
+    onPageLoad: function(aEvent) {
+        var doc = aEvent.originalTarget;
+        if ((doc instanceof HTMLDocument) && (rdf = $('#tipsy'))){
+            //Try to parse the RDFa out of the HTML
+			
+		}
+        
+    },
+    
+};
+*/
+
+var myExt_urlBarListener = {  
+   QueryInterface: function(aIID)  
+   {  
+    if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||  
+        aIID.equals(Components.interfaces.nsISupportsWeakReference) ||  
+        aIID.equals(Components.interfaces.nsISupports))  
+      return this;  
+    throw Components.results.NS_NOINTERFACE;  
+   },  
+   
+   onLocationChange: function(aProgress, aRequest, aURI)  
+   {  
+     myExtension.processNewURL(aURI);  
+   },  
+   
+   onStateChange: function(a, b, c, d) {},  
+   onProgressChange: function(a, b, c, d, e, f) {},  
+   onStatusChange: function(a, b, c, d) {},  
+   onSecurityChange: function(a, b, c) {}  
+ };
+
+function switchbuttons() {
+	var redbutton = document.getElementById('vendor-button-active-rdfa');
+	var greybutton = document.getElementById('vendor-button-no-rdfa');
+	
+	if ( redbutton.style.display != 'none' ) {
+		greybutton.style.display = '';
+		redbutton.style.display = 'none';
+	}
+	else {
+		redbutton.style.display = '';
+		greybutton.style.display = 'none';
+	}
+}; 
+   
+ var myExtension = {  
+   oldURL: null,  
+     
+   init: function() {  
+     // Listens for webpage loads  
+     gBrowser.addProgressListener(myExt_urlBarListener,  
+         Components.interfaces.nsIWebProgress.NOTIFY_LOCATION);  
+   },  
+     
+   uninit: function() {  
+     gBrowser.removeProgressListener(myExt_urlBarListener);  
+   },
+   
+
+   processNewURL: function(aURI) {  
+     if (aURI.spec == this.oldURL)  
+       return;  
+       
+     // need to add code to check for RDF here
+	 
+	 switchbuttons();
+	 
+   },
+  
+   
+ };  
+   
+ window.addEventListener("load", function() {myExtension.init()}, false);  
+ window.addEventListener("unload", function() {myExtension.uninit()}, false);
