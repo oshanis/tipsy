2caef5f3778f2b12dacc4145914e1889a3bd5595
ahmad.bakhiet <ahmad.bakhiet@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-08-15 17:48:54 +0000

    Improved checkrdfa.js code and removed tipsy.xul file (as a sidebar is no longer required)

--- trunk/firefox/chrome/content/checkrdfa.js
+++ trunk/firefox/chrome/content/checkrdfa.js
@@ -1,30 +1,3 @@
-/*
-//Have to set the correct context for Jquery to pick things up
-jQuery.noConflict();
-$ = function(selector,context){ return new jQuery.fn.init(selector,context||window.content.document); };
-$.fn = $.prototype = jQuery.fn;
-
-var tipsy = {
-
-    db: null,
-
-    init: function() {
-        var appcontent = top.document.getElementById("appcontent");
-        if(appcontent) {
-            appcontent.addEventListener("DOMContentLoaded", tipsy.onPageLoad, true);
-        }
-    },
-    onPageLoad: function(aEvent) {
-        var doc = aEvent.originalTarget;
-        if ((doc instanceof HTMLDocument) && (rdf = $('#tipsy'))){
-            //Try to parse the RDFa out of the HTML
-			
-		}
-        
-    },
-    
-};
-*/
 
 var myExt_urlBarListener = {  
    QueryInterface: function(aIID)  
@@ -47,17 +20,19 @@ var myExt_urlBarListener = {
    onSecurityChange: function(a, b, c) {}  
  };
 
-function switchbuttons() {
+function rdfaStatus() {
 	var redbutton = document.getElementById('vendor-button-active-rdfa');
 	var greybutton = document.getElementById('vendor-button-no-rdfa');
 	
-	if ( redbutton.style.display != 'none' ) {
-		greybutton.style.display = '';
-		redbutton.style.display = 'none';
-	}
-	else {
+	if (true) {
+	// currently the above line of code always executes to true, I need to replace this so that it checks for rdfa on the page
+    //if (doc instanceof HTMLDocument){ //rdfa found:
 		redbutton.style.display = '';
 		greybutton.style.display = 'none';
+	}
+	else {
+		greybutton.style.display = '';
+		redbutton.style.display = 'none';
 	}
 }; 
    
@@ -74,18 +49,14 @@ function switchbuttons() {
      gBrowser.removeProgressListener(myExt_urlBarListener);  
    },
    
-
    processNewURL: function(aURI) {  
      if (aURI.spec == this.oldURL)  
        return;  
-       
-     // need to add code to check for RDF here
-	 
-	 switchbuttons();
+    
+	 rdfaStatus();
 	 
    },
   
-   
  };  
    
  window.addEventListener("load", function() {myExtension.init()}, false);  
