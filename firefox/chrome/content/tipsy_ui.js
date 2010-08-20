a9ca14b2fbd73ad7f2f316eeb20574c68a24e1f8
oshanis <oshanis@8e02f5b2-d61a-11de-89a3-7d4a0ca7bf7e>
2010-08-20 21:10:10 +0000

    cleared some alerts and changed the tipsy title to emancy

--- firefox/chrome/content/tipsy_ui.js
+++ firefox/chrome/content/tipsy_ui.js
@@ -51,7 +51,7 @@ refreshTable = function(){
   
 /** Clears the tipsy history containing the sites visited by the user, and sites liked by the user**/
 clearHistory = function(){
-    alert("clearHistory: Not Implemented!");
+    //alert("clearHistory: Not Implemented!");
 }
 
 /** Tempoarary function to manully add data to the list**/
@@ -62,35 +62,35 @@ myAppendItem = function(){
 /** This function should validate the number of sites the user wishes to select through the lottery process
 Validate for high numbers like 99 and for non numbers**/
 function validateNumberOfSitesToPay(){
-    alert("validateNumberOfSitesToPay: Not Implemented!");
+    //alert("validateNumberOfSitesToPay: Not Implemented!");
 }
 
 
 /**Once the user clicks on the disabled paypal account text box, this shoule be called, and it should verify with the user whether she really wants to change the PayPal account or not, and handle that event.**/
 function changePayPalAccount(){
-    alert("changePayPalAccount: Not Implemented");
+    //alert("changePayPalAccount: Not Implemented");
 }
 
 /**Run some randomized algo, and pick the winners**/
 function runElection(){
-    alert("runElection: Not Implemented");
+    //alert("runElection: Not Implemented");
 }
 
 /**Validate for negative amounts. Also, may be currency convertions?*/
 function validateWebWorth(){
-    alert("validateWebWorth: Not Implemented");
+    //alert("validateWebWorth: Not Implemented");
 }
 
 /**
 * Validate the web worth field, the frequency of running the election (may be not required), and the PayPal account. Upon success call the relevant database update methods, otherwise notify the problem(s) to the user
 **/
 function updateInfo (){
-    alert("updateInfo: Not Implemented");
+   // alert("updateInfo: Not Implemented");
 }
 
 /**Validate the frequency of which the election is being run
 @@ We might not really need this method!
 **/
 function validateFrequency(){
-    alert("validateFrequency: Not Implemented");
+   // alert("validateFrequency: Not Implemented");
 }
